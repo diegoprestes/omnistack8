84044de4d8316a1d139e7d46bd2d8f96ec73d870
Diego Prestes <diego.prestes@possible.com>
2019-08-20 15:09:05 -0300

    mobile update

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+} 
+
+// 25:36
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
