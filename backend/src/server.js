f7e194154bee495179e8e114b9aa9381165e1186
Diego Prestes <diego.prestes@possible.com>
2019-09-02 11:10:56 -0300

    socket communication added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,24 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const connectedUsers = {
+  
+};
+
 io.on('connection', socket => {
-  console.log('Nova conexao', socket.id);
-  // 6:36
+  const { user } = socket.handshake.query;
+  connectedUsers[user] = socket.id;
+
+  // socket io how to use examples
+  // socket.on('hello', message => {
+  //   console.log('Received: ', message);
+  // });
+
+  // setTimeout(() => {
+  //   socket.emit('world', {
+  //     message: 'Send to frontend'
+  //   });
+  // }, 3000);
 });
 
 const mongooseURL = 'mongodb+srv://diego:diego@flashprestesapi-tqbua.mongodb.net/omnistack8?retryWrites=true&w=majority';
@@ -18,6 +33,14 @@ mongoose.connect(mongooseURL, { useNewUrlParser: true })
   .then(() => {
     console.info("DB Connected");
   });
+
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 
 import api from '../services/api';
@@ -24,6 +25,29 @@ export default function Main({ match }) {
     }
 
     loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      console.log('Match: ', dev);
+      // 23:27
+    });
+
+    // socket io how to use examples
+    // socket.on('world', message => {
+    //   console.log('Received: ', message);
+    // });
+
+    // setTimeout(() => {
+    //   socket.emit('hello', {
+    //     message: 'Send to backend'
+    //   });
+    // }, 3000);
+
   }, [match.params.id]);
 
   async function handleLike(id) {
